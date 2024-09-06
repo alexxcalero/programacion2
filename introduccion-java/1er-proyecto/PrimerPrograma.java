319167a9a65f77f50ab82d53c06d1c9ea8e45f2d
Alex Calero Revilla <alexcalerorevilla@gmail.com>
2024-09-06 00:51:15 -0500

    Primer programa en java

---
+++ introduccion-java/1er-proyecto/PrimerPrograma.java
@@ -0,0 +1,12 @@
+public class PrimerPrograma {
+	public static void main(String[] args) {
+		int edad = 24;
+		double altura = 1.75;
+		String nombre = "Alex Calero Revilla";
+		boolean activo = true;
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Edad: " + edad);
+		System.out.println("Altura: " + altura);
+		System.out.println("Activo: " + activo);
+	}
+}
